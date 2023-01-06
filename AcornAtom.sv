b6790d008400abc98ac2defb3fc50d4936839884
Alan Steremberg <alan.steremberg@gmail.com>
2023-01-06 11:22:56 -0800

    updated sys

--- AcornAtom.sv
+++ AcornAtom.sv
@@ -28,7 +28,7 @@ module emu
 	input         RESET,
 
 	//Must be passed to hps_io module
-	inout  [47:0] HPS_BUS,
+	inout  [48:0] HPS_BUS,
 
 	//Base video clock. Usually equals to CLK_SYS.
 	output        CLK_VIDEO,
@@ -51,13 +51,14 @@ module emu
 	output        VGA_F1,
 	output [1:0]  VGA_SL,
 	output        VGA_SCALER, // Force VGA scaler
+	output        VGA_DISABLE, // analog out is off
 
 	input  [11:0] HDMI_WIDTH,
 	input  [11:0] HDMI_HEIGHT,
 	output        HDMI_FREEZE,
 
 `ifdef MISTER_FB
-	// Use framebuffer in DDRAM (USE_FB=1 in qsf)
+	// Use framebuffer in DDRAM
 	// FB_FORMAT:
 	//    [2:0] : 011=8bpp(palette) 100=16bpp 101=24bpp 110=32bpp
 	//    [3]   : 0=16bits 565 1=16bits 1555
@@ -172,6 +173,7 @@ module emu
 	input         OSD_STATUS
 );
 
+
 assign ADC_BUS  = 'Z;
 assign USER_OUT = '1;
 assign UART_RTS = UART_CTS;
@@ -184,6 +186,7 @@ assign LED_DISK  = {1'b1,~vsd_sel & sd_act};
 assign LED_POWER = 0;
 assign BUTTONS   = 0;
 
+assign VGA_DISABLE = 0;
 assign VGA_SCALER = 0;
 assign HDMI_FREEZE = 0;
 
@@ -332,7 +335,7 @@ hps_io #(.CONF_STR(CONF_STR)) hps_io
 	.img_size(img_size),
 
 	.joystick_0(joy1),
-	.joystick_1(joy2),
+	.joystick_1(joy2)
 );
 
 /////////////////  RESET  /////////////////////////
